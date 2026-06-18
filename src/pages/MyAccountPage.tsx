65a1641f438e0540efdbcdcf9a54eb6bd4ef9fe2
ali-primer <mousavia465@gmail.com>
2026-06-18 11:08:58 +0330

    feat: upate icons on account page

--- src/pages/MyAccountPage.tsx
+++ src/pages/MyAccountPage.tsx
@@ -18,7 +18,7 @@ type AccountAction = {
 
 type AccountIconName =
   | "agency"
-  | "article"
+  | "headphone"
   | "bookmark"
   | "building"
   | "dashboard"
@@ -27,6 +27,7 @@ type AccountIconName =
   | "info"
   | "legal"
   | "lock"
+  | "log_out"
   | "note"
   | "plus"
   | "ranking"
@@ -70,7 +71,7 @@ const secondaryActions: AccountAction[] = [
   { icon: "setting", label: "تنظیمات", to: "/account/about" },
   { icon: "info", label: "درباره ما", to: "/account/about" },
   { icon: "legal", label: "ضوابط و قوانین", to: "/account/about" },
-  { icon: "article", label: "مقالات تخصصی املاک", to: "/account/about" },
+  { icon: "headphone", label: "پشتیبانی", to: "/account/about" },
 ];
 
 const loggedOutSecondaryActions: AccountAction[] = [
@@ -151,8 +152,8 @@ function IndependentConsultantAccountPage() {
       <AccountSection
         actions={[
           {
-            icon: "lock",
-            label: isLoggingOut ? "در حال خروج..." : "خروج از حساب",
+            icon: "log_out",
+            label: isLoggingOut ? "در حال خروج..." : "خروج از حساب کاربری",
             onClick: handleLogout,
           },
         ]}
@@ -175,12 +176,12 @@ function StandardAccountPage({ authSession }: { authSession: AuthSession | null 
       {isLoggedInUnverified ? (
         <section className="bg-white" aria-label="وضعیت حساب">
           <div className="flex h-32 items-center gap-4 px-4 [direction:rtl]">
-            <div className="grid h-[72px] w-[72px] shrink-0 place-items-center rounded-full bg-[#e0e0e0] text-[#808080]">
-              <AccountIcon name="user" className="h-8 w-8" />
+            <div className="grid p-5 shrink-0 place-items-center rounded-full bg-[#e0e0e0] text-[#808080]">
+              <AccountIcon name="user" color="#CCCCCC" className="h-8 w-8" />
             </div>
 
             <div className="min-w-0 flex-1 text-right">
-              <p className="m-0 text-sm font-semibold leading-5 text-[#0048c4]">
+              <p className="m-0 text-sm font-semibold leading-5 text-[#0048C4]">
                 احراز هویت نشده
               </p>
               <p className="m-0 mt-2 text-sm font-medium leading-5 text-[#808080] [direction:ltr]">
@@ -190,34 +191,34 @@ function StandardAccountPage({ authSession }: { authSession: AuthSession | null 
           </div>
           <Divider />
         </section>
-        ) : (
-          <LoggedOutAccountHeader />
-        )}
+      ) : (
+        <LoggedOutAccountHeader />
+      )}
 
-        <AccountSection actions={isLoggedInUnverified ? businessActions : loggedOutBusinessActions} />
+      <AccountSection actions={isLoggedInUnverified ? businessActions : loggedOutBusinessActions} />
 
-        <div className="h-4 bg-[#f0f0f0]" />
+      <div className="h-4 bg-[#f0f0f0]" />
 
-        <AccountSection actions={isLoggedInUnverified ? primaryActions : loggedOutPrimaryActions} />
+      <AccountSection actions={isLoggedInUnverified ? primaryActions : loggedOutPrimaryActions} />
 
-        <div className="h-4 bg-[#f0f0f0]" />
+      <div className="h-4 bg-[#f0f0f0]" />
 
-        <AccountSection actions={isLoggedInUnverified ? secondaryActions : loggedOutSecondaryActions} />
+      <AccountSection actions={isLoggedInUnverified ? secondaryActions : loggedOutSecondaryActions} />
 
-        {isLoggedInUnverified ? (
-          <>
-            <div className="h-4 bg-[#f0f0f0]" />
-            <AccountSection
-              actions={[
-                {
-                  icon: "lock",
-                  label: isLoggingOut ? "در حال خروج..." : "خروج از حساب",
-                  onClick: handleLogout,
-                },
-              ]}
-            />
-          </>
-        ) : null}
+      {isLoggedInUnverified ? (
+        <>
+          <div className="h-4 bg-[#f0f0f0]" />
+          <AccountSection
+            actions={[
+              {
+                icon: "log_out",
+                label: isLoggingOut ? "در حال خروج..." : "خروج از حساب",
+                onClick: handleLogout,
+              },
+            ]}
+          />
+        </>
+      ) : null}
     </TopBarNavigationLayout>
   );
 }
@@ -237,7 +238,7 @@ function useLogoutAccount() {
 
     logoutMutation.mutate(undefined, {
       onSettled: () => {
-      navigateTo("/login/phone");
+        navigateTo("/login/phone");
       },
     });
   };
@@ -380,9 +381,11 @@ function NotificationIcon({ className = "" }: { className?: string }) {
 function AccountIcon({
   className = "",
   name,
+  color,
 }: {
   className?: string;
   name: AccountIconName;
+  color?: string
 }) {
   return (
     <svg
@@ -402,18 +405,19 @@ function AccountIcon({
           <path d="M8 8h.01M12 8h.01M16 8h.01M8 12h.01M16 12h.01" />
         </>
       ) : null}
-      {name === "article" ? (
+      {name === "headphone" ? (
         <>
-          <path d="M4 5.5A2.5 2.5 0 0 1 6.5 3H20v18H6.5A2.5 2.5 0 0 1 4 18.5v-13Z" />
-          <path d="M8 7h8M8 11h8M8 15h5" />
+          <path xmlns="http://www.w3.org/2000/svg" d="M5 9C5 5.68629 8.13401 3 12 3C15.866 3 19 5.68629 19 9M19 17V19C19 20.1046 18.1046 21 17 21H12M4.10496 9.59854L2.4458 10.7032C2.16731 10.8886 2 11.201 2 11.5356V14.4644C2 14.799 2.16731 15.1114 2.4458 15.2968L4.10496 16.4015C4.66094 16.7716 5.37255 16.8158 5.97002 16.5172C6.60124 16.2017 7 15.5566 7 14.851V11.149C7 10.4434 6.60124 9.7983 5.97002 9.48283C5.37255 9.18422 4.66094 9.22837 4.10496 9.59854ZM18.03 9.48283C18.6274 9.18422 19.3391 9.22837 19.895 9.59854L21.5542 10.7032C21.8327 10.8886 22 11.201 22 11.5356V14.4644C22 14.799 21.8327 15.1114 21.5542 15.2968L19.895 16.4015C19.3391 16.7716 18.6274 16.8158 18.03 16.5172C17.3988 16.2017 17 15.5566 17 14.851V11.149C17 10.4434 17.3988 9.7983 18.03 9.48283Z" stroke="#4D4D4D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
         </>
       ) : null}
-      {name === "bookmark" ? <path d="M7 4h10v16l-5-3-5 3V4Z" /> : null}
+      {name === "bookmark" ?
+        <>
+          <path xmlns="http://www.w3.org/2000/svg" d="M12 17L19 21V5C19 4 18 3 17 3H7C6 3 5 4 5 5V21L12 17Z" stroke="#4D4D4D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        </>
+        : null}
       {name === "building" ? (
         <>
-          <path d="M4 21V5l10 3v13" />
-          <path d="M14 12h6v9" />
-          <path d="M8 9h2M8 13h2M8 17h2M17 16h.01" />
+          <path xmlns="http://www.w3.org/2000/svg" d="M8.4 21V9.3C8.4 8.80295 8.80295 8.4 9.3 8.4H15.6C16.0971 8.4 16.5 8.80295 16.5 9.3V21M11.55 12.0016H13.35M11.55 14.7008H13.35M11.55 17.4016L13.35 17.4M3.9 20.9947V12.8947C3.9 12.3977 4.30295 11.9947 4.8 11.9947H8.4M21 20.9895L3 21M12.9 8.37306V3L20.1 5.75523V21" stroke="#4D4D4D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
         </>
       ) : null}
       {name === "dashboard" ? (
@@ -426,8 +430,8 @@ function AccountIcon({
       ) : null}
       {name === "eye" ? (
         <>
-          <path d="M2.5 12s3.5-6 9.5-6 9.5 6 9.5 6-3.5 6-9.5 6-9.5-6-9.5-6Z" />
-          <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+          <path d="M2.5 12s3.5-6.5 9.5-6.5 9.5 6.5 9.5 6.5-3.5 6.5-9.5 6.5S2.5 12 2.5 12Z" />
+          <circle cx="12" cy="12" r="2.75" />
         </>
       ) : null}
       {name === "identity" ? (
@@ -445,9 +449,7 @@ function AccountIcon({
       ) : null}
       {name === "legal" ? (
         <>
-          <path d="M12 3v18M6 7h12" />
-          <path d="M6 7 3.5 13h5L6 7ZM18 7l-2.5 6h5L18 7Z" />
-          <path d="M8 21h8" />
+          <path xmlns="http://www.w3.org/2000/svg" d="M11.5 10.5005L8.5 13.5M8.5 18H15.5M3 5V19C3 20.6569 4.34315 22 6 22H18C19.6569 22 21 20.6569 21 19V5C21 3.34315 19.6569 2 18 2H6C4.34315 2 3 3.34315 3 5ZM12.0002 6.00012L16.0005 10.0005L13.5004 12.5006L9.5 8.5003L12.0002 6.00012Z" stroke="#4D4D4D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
         </>
       ) : null}
       {name === "lock" ? (
@@ -456,10 +458,16 @@ function AccountIcon({
           <path d="M8 10V7a4 4 0 0 1 8 0v3" />
         </>
       ) : null}
+      {name === "log_out" ? (
+        <>
+          <path xmlns="http://www.w3.org/2000/svg" d="M6.66667 7.05002C5.03 8.53319 4 10.501 4 12.9C4 17.3735 7.58172 21 12 21C16.4183 21 20 17.3735 20 12.9C20 10.501 18.97 8.53319 17.3333 7.05002M9.77778 5.25001L12 3L14.2222 5.25001M12 12V3.5479" stroke="#4D4D4D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        </>
+      ) : null}
       {name === "note" ? (
         <>
-          <rect height="16" rx="2" width="14" x="5" y="4" />
-          <path d="M9 3v3M15 3v3M8.5 10h7M8.5 14h5" />
+          <path d="M7 3v3M12 3v3M17 3v3" />
+          <path d="M6.5 4.5h11A2.5 2.5 0 0 1 20 7v11.5A2.5 2.5 0 0 1 17.5 21h-11A2.5 2.5 0 0 1 4 18.5V7a2.5 2.5 0 0 1 2.5-2.5Z" />
+          <path d="M8 10h8M8 14h5" />
         </>
       ) : null}
       {name === "plus" ? <path d="M12 5v14M5 12h14" /> : null}
@@ -471,32 +479,31 @@ function AccountIcon({
       ) : null}
       {name === "request" ? (
         <>
-          <path d="M6 3h10l3 3v15H6V3Z" />
-          <path d="M16 3v4h4M9 11h6M9 15h4M17 17l2 2 3-4" />
+          <path xmlns="http://www.w3.org/2000/svg" d="M21 15V6C21 4.89543 20.1046 4 19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H11M7 9L17 9M7 13H12M7 17H7.01M21 19.5H15M16.5 22L14 19.5L16.5 17" stroke="#4D4D4D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
         </>
       ) : null}
       {name === "setting" ? (
         <>
-          <path d="M12 15.2a3.2 3.2 0 1 0 0-6.4 3.2 3.2 0 0 0 0 6.4Z" />
-          <path d="M19.4 15a1.7 1.7 0 0 0 .34 1.87l.06.06a2.05 2.05 0 0 1-2.9 2.9l-.06-.06a1.7 1.7 0 0 0-1.87-.34 1.7 1.7 0 0 0-1.03 1.56V21a2.05 2.05 0 0 1-4.1 0v-.09a1.7 1.7 0 0 0-1.11-1.56 1.7 1.7 0 0 0-1.87.34l-.06.06a2.05 2.05 0 0 1-2.9-2.9l.06-.06A1.7 1.7 0 0 0 4.3 15a1.7 1.7 0 0 0-1.56-1.03h-.09a2.05 2.05 0 0 1 0-4.1h.09A1.7 1.7 0 0 0 4.3 8.74a1.7 1.7 0 0 0-.34-1.87L3.9 6.8a2.05 2.05 0 0 1 2.9-2.9l.06.06A1.7 1.7 0 0 0 8.73 4.3h.08a1.7 1.7 0 0 0 1.03-1.56v-.09a2.05 2.05 0 0 1 4.1 0v.09a1.7 1.7 0 0 0 1.03 1.56 1.7 1.7 0 0 0 1.87-.34l.06-.06a2.05 2.05 0 0 1 2.9 2.9l-.06.06a1.7 1.7 0 0 0-.34 1.87v.08a1.7 1.7 0 0 0 1.56 1.03h.09a2.05 2.05 0 0 1 0 4.1h-.09A1.7 1.7 0 0 0 19.4 15Z" />
+          <path d="M12 15.25a3.25 3.25 0 1 0 0-6.5 3.25 3.25 0 0 0 0 6.5Z" />
+          <path d="M19.4 13.5a7.7 7.7 0 0 0 .05-3l2-1.55-2-3.45-2.45 1a7.4 7.4 0 0 0-2.6-1.5L14 2.4h-4l-.4 2.6A7.4 7.4 0 0 0 7 6.5l-2.45-1-2 3.45 2 1.55a7.7 7.7 0 0 0 .05 3l-2.05 1.55 2 3.45L7 17.5a7.4 7.4 0 0 0 2.6 1.5l.4 2.6h4l.4-2.6a7.4 7.4 0 0 0 2.6-1.5l2.45 1 2-3.45-2.05-1.55Z" />
         </>
       ) : null}
       {name === "tag" ? (
         <>
-          <path d="M20 13 13 20 4 11V4h7l9 9Z" />
-          <path d="M8.5 8.5h.01" />
+          <path xmlns="http://www.w3.org/2000/svg" d="M7.83617 13.3498L10.6529 16.1664M17.712 7.69446C17.712 6.91665 17.0814 6.28612 16.3037 6.28612C15.5259 6.28612 14.8953 6.91665 14.8953 7.69446C14.8953 8.47226 15.5259 9.10279 16.3037 9.10279C17.0814 9.10279 17.712 8.47226 17.712 7.69446ZM10.2833 20.45L3.54999 13.7167C2.81668 12.9834 2.81667 11.7944 3.54999 11.0611L11.0611 3.54999C11.4133 3.19783 11.8909 3 12.3889 3H21V11.6111C21 12.1091 20.8022 12.5867 20.45 12.9389L12.9389 20.45C12.2056 21.1833 11.0166 21.1833 10.2833 20.45Z" stroke="#4D4D4D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
         </>
       ) : null}
       {name === "user" ? (
         <>
-          <path d="M12 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z" />
-          <path d="M4.5 21a7.5 7.5 0 0 1 15 0" />
+          <path d="M13 14H11C7.13401 14 4 17.134 4 21H20C20 17.134 16.866 14 13 14Z" stroke={color ? color : '#4D4D4D'} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M12 11C14.2091 11 16 9.20914 16 7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7C8 9.20914 9.79086 11 12 11Z" stroke={color ? color : '#4D4D4D'} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
         </>
       ) : null}
       {name === "wallet" ? (
         <>
-          <path d="M4 7.5A2.5 2.5 0 0 1 6.5 5H19v14H6.5A2.5 2.5 0 0 1 4 16.5v-9Z" />
-          <path d="M17 12h3v4h-3a2 2 0 0 1 0-4Z" />
+          <path d="M4 7.5h13.25A2.75 2.75 0 0 1 20 10.25v7A2.75 2.75 0 0 1 17.25 20H6.75A2.75 2.75 0 0 1 4 17.25V6.75A2.75 2.75 0 0 1 6.75 4h8.75A1.5 1.5 0 0 1 17 5.5v2" />
+          <path d="M15 12.25h5v4h-5a2 2 0 0 1 0-4Z" />
+          <path d="M17 14.25h.01" />
         </>
       ) : null}
       {name === "wallet-add" ? (
